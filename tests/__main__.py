16956b9f9f17eebe43efe259c36e93fdf5ad125f
Timur Pavlov <pavlov.timur556@yandex.ru>
2022-10-23 15:52:23 +0300

    fix: bot tests fix

--- tests/__main__.py
+++ tests/__main__.py
@@ -1,7 +1,7 @@
 import unittest
 
 suite = unittest.TestSuite()
-test_modules = ["tests.test_bot"]
+test_modules = ["tests.test_bot", "tests.test_dataset_item", "tests.test_args_parser"]
 for t in test_modules:
     try:
         mod = __import__(t, globals(), locals(), ["suite"])
